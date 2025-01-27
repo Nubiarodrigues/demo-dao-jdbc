41acd3e4f804d090b4c7261c79b730c7ca5d7c24
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-01-27 11:03:47 -0300

    Interfaces departmentDao e sellerDao

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,19 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public interface DepartmentDao {
+
+	void insert(Department obj);
+
+	void update(Department obj);
+
+	void deleteById(Integer id);
+
+	Department findById(Integer id); // consultar 
+
+	List<Department> findAll(); // retornar todos os departamentos
+
+}
