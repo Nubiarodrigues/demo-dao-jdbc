ce8fed6d74b9034d0eca8a92ea4b87c04e9ccfcf
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-01-28 10:58:53 -0300

    Implementação do departmentDaoJDBC

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -7,13 +7,9 @@ import model.entities.Department;
 public interface DepartmentDao {
 
 	void insert(Department obj);
-
 	void update(Department obj);
-
 	void deleteById(Integer id);
-
 	Department findById(Integer id); // consultar 
-
 	List<Department> findAll(); // retornar todos os departamentos
 
 }
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -8,13 +8,9 @@ import model.entities.Seller;
 public interface SellerDao {
 
 	void insert(Seller obj);
-
 	void update(Seller obj);
-
 	void deleteById(Integer id);
-
-	Seller findById(Integer id); // consultar
-
-	List<Seller> findAll(); // retornar todos os vendedores
+	Seller findById(Integer id); 
+	List<Seller> findAll(); 
 	List<Seller> findByDepartment(Department department);
 }
